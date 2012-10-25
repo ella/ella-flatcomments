dbad63f5117de8daa616dcec6658f046be55b4cc
Honza Kral <honza.kral@gmail.com>
2012-10-25 20:45:04 +0200

    LastCommentedListingHandler uses normal datetimes

--- ella_flatcomments/listing_handlers.py
+++ ella_flatcomments/listing_handlers.py
@@ -4,7 +4,7 @@ from ella.utils.timezone import to_timestamp
 
 from ella_flatcomments.models import CommentList
 
-class CommentListingHandlerMixin(object):
+class CommentCountListingHandlerMixin(object):
     def _get_score_limits(self):
         max_score = None
         min_score = None
@@ -16,11 +16,11 @@ class CommentListingHandlerMixin(object):
     def _get_listing(self, publishable, score):
         return Listing(publishable=publishable, publish_from=publishable.publish_from)
 
-class RecentMostCommentedListingHandler(CommentListingHandlerMixin, SlidingListingHandler):
+class RecentMostCommentedListingHandler(CommentCountListingHandlerMixin, SlidingListingHandler):
     PREFIX = 'recent_comcount'
 
 
-class MostCommentedListingHandler(CommentListingHandlerMixin, RedisListingHandler):
+class MostCommentedListingHandler(CommentCountListingHandlerMixin, RedisListingHandler):
     PREFIX = 'comcount'
 
     @classmethod
@@ -30,8 +30,9 @@ class MostCommentedListingHandler(CommentListingHandlerMixin, RedisListingHandle
         super(MostCommentedListingHandler, cls).add_publishable(category, publishable, score, pipe=pipe, commit=commit)
 
 
-class LastCommentedListingHandler(CommentListingHandlerMixin, RedisListingHandler):
+class LastCommentedListingHandler(RedisListingHandler):
     PREFIX = 'lastcommented'
+
     @classmethod
     def add_publishable(cls, category, publishable, score=None, pipe=None, commit=True):
         if score is None:
