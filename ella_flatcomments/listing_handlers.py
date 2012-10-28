985606962c3871c2df20c31fcbecf2a16a9aaa55
Honza Kral <honza.kral@gmail.com>
2012-10-28 02:55:21 +0200

    use ella's TimeBasedListingHandler

--- ella_flatcomments/listing_handlers.py
+++ ella_flatcomments/listing_handlers.py
@@ -1,26 +1,12 @@
-from ella.core.models import Listing
-from ella.core.cache.redis import RedisListingHandler, SlidingListingHandler
-from ella.utils.timezone import to_timestamp
+from ella.core.cache.redis import RedisListingHandler, SlidingListingHandler, TimeBasedListingHandler
 
 from ella_flatcomments.models import CommentList
 
-class CommentCountListingHandlerMixin(object):
-    def _get_score_limits(self):
-        max_score = None
-        min_score = None
-
-        if self.date_range:
-            raise NotSupported()
-        return min_score, max_score
-
-    def _get_listing(self, publishable, score):
-        return Listing(publishable=publishable, publish_from=publishable.publish_from)
-
-class RecentMostCommentedListingHandler(CommentCountListingHandlerMixin, SlidingListingHandler):
+class RecentMostCommentedListingHandler(SlidingListingHandler):
     PREFIX = 'recent_comcount'
 
 
-class MostCommentedListingHandler(CommentCountListingHandlerMixin, RedisListingHandler):
+class MostCommentedListingHandler(RedisListingHandler):
     PREFIX = 'comcount'
 
     @classmethod
@@ -30,12 +16,12 @@ class MostCommentedListingHandler(CommentCountListingHandlerMixin, RedisListingH
         super(MostCommentedListingHandler, cls).add_publishable(category, publishable, score, pipe=pipe, commit=commit)
 
 
-class LastCommentedListingHandler(RedisListingHandler):
+class LastCommentedListingHandler(TimeBasedListingHandler):
     PREFIX = 'lastcommented'
 
     @classmethod
-    def add_publishable(cls, category, publishable, score=None, pipe=None, commit=True):
-        if score is None:
-            score = repr(to_timestamp(CommentList(publishable.content_type, publishable.pk)[0].submit_date))
-        super(LastCommentedListingHandler, cls).add_publishable(category, publishable, score, pipe=pipe, commit=commit)
+    def add_publishable(cls, category, publishable, score=None, publish_from=None, pipe=None, commit=True):
+        if score is None and publish_from is None:
+            publish_from = CommentList(publishable.content_type, publishable.pk)[0].submit_date
+        super(LastCommentedListingHandler, cls).add_publishable(category, publishable, score=score, publish_from=publish_from, pipe=pipe, commit=commit)
 
