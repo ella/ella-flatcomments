30e6c11935592e42f01099f40a3e451267375c11
Honza Král <honza.kral@gmail.com>
2015-05-01 15:58:58 +0200

    Newer Django compatibility

--- ella_flatcomments/urls.py
+++ ella_flatcomments/urls.py
@@ -1,6 +1,9 @@
 from django.template.defaultfilters import slugify
 from django.utils.translation import ugettext as _
-from django.conf.urls.defaults import patterns, url
+try:
+    from django.conf.urls import patterns, url
+except ImportError:
+    from django.conf.urls.defaults import patterns, url
 
 from ella_flatcomments.views import list_comments, post_comment, comment_detail, moderate_comment, lock_comments, unlock_comments
 
